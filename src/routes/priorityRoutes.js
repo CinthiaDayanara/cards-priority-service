014c2bad51be0a40f6aff945aecda9b74cc80e82
CinthiaDayanara <cdchuraco@uce.edu.ec>
2025-02-12 18:00:29 -0500

    fff

--- src/routes/priorityRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getCards } = require("../controllers/priorityController");
-const router = express.Router();
-
-// Ruta GET para obtener todas las tarjetas
-router.get("/", getCards);
-
-module.exports = router;
